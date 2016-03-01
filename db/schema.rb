967950e1203880505f2ca91c86b7a2d05c1cca22
Ash Tyndall <ash@icetana.com>
2016-03-01 17:09:32 +0800

    Reverse startdate removal (I think Mark wants this?)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228090010) do
+ActiveRecord::Schema.define(version: 20160228021943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160228090010) do
   create_table "membership_requests", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "membership_type_id"
+    t.date     "startdate"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "workflow_state"
