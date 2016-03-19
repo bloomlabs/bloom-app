50056249c5227d53e1a8ab7e364a745206a5f82c
Ash Tyndall <ash@icetana.com>
2016-03-19 19:20:41 +0800

    Move logging back to debug to catch error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Info log level
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
