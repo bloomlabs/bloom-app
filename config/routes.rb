747d20b6c83ea71a05d645e882277726553e2c6c
hsmallbone <21306592@student.uwa.edu.au>
2016-04-16 22:20:54 +0800

    Add get variants for routes to work around iOS problems

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   scope 'api', controller: :api do
     post 'user_auth_token'
+    get 'user_auth_token'
+    get 'profiles/:id' => 'api#get_profile_info'
     post 'profiles/:id' => 'api#get_profile_info'
   end
 
