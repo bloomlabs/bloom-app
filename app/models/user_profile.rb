5840c8c8f81ffe41cd67b476e517ea9defa4a28a
hsmallbone <21306592@student.uwa.edu.au>
2016-08-17 11:20:58 +0800

    Never mind

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,7 +3,7 @@ class UserProfile < ActiveRecord::Base
   belongs_to :user
 
   def name
-    "#{user.firstname} #{user.lastname} @ #{created_at}"
+    "#{user.name} @ #{created_at}"
   end
 
   rails_admin do
